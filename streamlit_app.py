89d2ec27af12153578734b05e898b1faa6c80f05
Andrew Hlavacek <86272039+AndrewHlavacek@users.noreply.github.com>
2025-06-02 11:46:33 -0400

    Update streamlit_app.py
    spacing for infor

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
         if smoothiefroot_response.status_code == 200:
