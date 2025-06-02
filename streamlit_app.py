7798f4d3a767c3792f46182e6e6f264121102dcd
Andrew Hlavacek <86272039+AndrewHlavacek@users.noreply.github.com>
2025-06-02 11:13:10 -0400

    Update streamlit_app.py
    trying to change issues currently with streamlit hosting and connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark import Session
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
@@ -13,8 +14,10 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 # Pulls from data base to get files uploaded from stage
-cnx = st.session("snowflake")
-session = cnx.session()
+
+connection_parameters = st.secrets["connections"]["snowflake"]
+session = Session.builder.configs(connection_parameters).create()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
